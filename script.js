8aebf242614096a4f1759e90c564bf765610177f
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-08 18:57:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function firstWord(s) {
-  // your code here
+function firstWord(s){
+    if(!s.trim()){
+        return ' ';
+    }
+    let str=s.trim();
+    let firstword='';
+    for(let i=0;i<s.length;i++){
+        if(str[i]===' '){
+            return firstword;
+        }
+        firstword+=str[i];
+    }
+   return firstword;
 }
-
-// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
